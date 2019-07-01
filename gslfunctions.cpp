70dbbdce49a6c9c1c999fb3385ab75b0a138aea6
Alejandra Torres Manotas <alejatorresm@gmail.com>
2019-07-01 18:04:46 -0500

    Delete gslfunctions.cpp

--- gslfunctions.cpp
+++
@@ -1,7 +0,0 @@
-#include "gslIntegrate.h"
-
-double examplefunction(double x, void* param)//Definition of the function with parametres
-{
- Params* p = (Params*)param;
- return p->a*sin(cos(p->v-p->w*x))+p->b*x-p->c*x*x;
-}
